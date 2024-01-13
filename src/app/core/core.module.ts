355adb665711b6c1bab1e0351e3cbffca3949e83
Jesiane Straubel <jesi1951@hotmail.com>
2024-01-13 18:07:06 -0300

    Implementando rodapé na aplicação

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,16 +4,17 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule } from '@angular/router';
 import { NgbCollapseModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AuthModule } from './auth/auth.module';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
-  declarations: [NavbarComponent],
+  declarations: [NavbarComponent, FooterComponent],
   imports: [
     CommonModule,
     RouterModule,
     NgbCollapseModule,
     AuthModule,
   ],
-  exports: [NavbarComponent, AuthModule],
+  exports: [NavbarComponent, AuthModule,FooterComponent],
 })
 export class CoreModule {}
